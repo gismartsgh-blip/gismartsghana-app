4686cdbafd792769cb5fcdaf5e15c0b389bf8f71
gismartsgh-blip <gismartsgh@gmail.com>
2025-10-29 21:54:04 +0000

    Delete firebaseConfig.js

--- firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAnnDCzBzJNVklEYUVNK7nhg8r7PZcktfo",
-  authDomain: "gi-smarts-ghana.firebaseapp.com",
-  projectId: "gi-smarts-ghana",
-  storageBucket: "gi-smarts-ghana.firebasestorage.app",
-  messagingSenderId: "255897033821",
-  appId: "1:255897033821:web:fbd21646482b40e5e941b4"
-};
